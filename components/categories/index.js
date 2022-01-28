ceb2719388c268b0087309ddf44d105d5ec0e180
ahsanz1 <ahsan.zafar@shopdev.co>
2022-01-28 20:18:01 +0500

    disabled es-lint

--- components/categories/index.js
+++ components/categories/index.js
@@ -6,9 +6,9 @@ import styles from "../categories/categories.module.css";
 const Categories = ({ categories }) => {
   return (
     <div className={styles["categories-container"]}>
-      {categories.map((category) => (
-        <Link href={`/c?category=${category.name}`}>
-          <div className={styles['category-item']}>
+      {categories.map((category, i) => (
+        <Link href={`/c?category=${category.name}`} key={i}>
+          <div className={styles["category-item"]}>
             <Image src={category.imageUrl} width={240} height={260} />
             <h3 className={styles["category-item-name"]}>{category.name}</h3>
           </div>
