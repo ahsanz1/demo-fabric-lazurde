49facd674d1e8bef55b43b38a8452e363e962b29
ahsanz1 <ahsan.zafar@shopdev.co>
2022-01-29 15:20:00 +0500

    env variables added

--- general-config.js
+++ general-config.js
@@ -1,9 +1,12 @@
-export const ACCOUNT = "60b63d9e7ce90800085548cf";
-export const DOMAIN = "https://uat01-apigw.klondike.fabric.zone";
-export const STAGE = "sandbox";
-export const CHANNEL = "12";
+export const ACCOUNT = process.env.ACCOUNT || "60b63d9e7ce90800085548cf";
+export const DOMAIN =
+  process.env.DOMAIN || "https://uat01-apigw.klondike.fabric.zone";
+export const STAGE = process.env.STAGE || "sandbox";
+export const CHANNEL = process.env.CHANNEL || "12";
 
 export const SEARCH_INDEX =
+  process.env.ALGOLIA_SEARCH_INDEX ||
   "60b63d9e7ce90800085548cf-sandbox-commerce-search-product";
-export const ALGOLIA_API_KEY = "406ddfaa655ec2e9d8f3fa337ff11502";
-export const ALGOLIA_APP_ID = "AWP6XJTOHO";
+export const ALGOLIA_API_KEY =
+  process.env.ALGOLIA_API_KEY || "406ddfaa655ec2e9d8f3fa337ff11502";
+export const ALGOLIA_APP_ID = process.env.ALGOLIA_API_KEY || "AWP6XJTOHO";
