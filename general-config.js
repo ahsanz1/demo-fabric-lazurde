8f7fc189dcbb1611fbc4e990385b22b35543ae32
ahsanz1 <ahsan.zafar@shopdev.co>
2022-01-29 15:44:44 +0500

    logs for algolia added

--- general-config.js
+++ general-config.js
@@ -9,4 +9,4 @@ export const SEARCH_INDEX =
   "60b63d9e7ce90800085548cf-sandbox-commerce-search-product";
 export const ALGOLIA_API_KEY =
   process.env.ALGOLIA_API_KEY || "406ddfaa655ec2e9d8f3fa337ff11502";
-export const ALGOLIA_APP_ID = process.env.ALGOLIA_API_KEY || "AWP6XJTOHO";
+export const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID || "AWP6XJTOHO";
